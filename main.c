1dd9e2a302051b0fc3a401edf20e43bcb8633444
Takeoischifan <takeoischifan@github.com>
2023-10-17 15:38:38 +0200

    bouncing ball

--- main.c
+++ main.c
@@ -1,20 +1,56 @@
-#include <stdio.h>
 #include "raylib.h"
+#include <stdio.h>
 
+// graphics constants
 #define SCREEN_SIZE 800
+#define LINE_THICKNESS 12
+#define LINE_COLOR BLACK
+#define BALL_SIZE 30
+
+// physics constants
 #define TARGET_FPS 60
+#define GRAVITY_PPF2 0.1
 
 
 int main(void){
 
+    const Vector2 shapes[] = {
+        {100, 700}, {700, 700},
+    };
+    const int num_lines = (sizeof(shapes) / sizeof(shapes[0])) / 2;
+
     InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Raysmr");
     SetTargetFPS(TARGET_FPS);
 
-    while (!WindowShouldClose())
-    {
+    Vector2 pos = {400, 400};
+    Vector2 vel = {0, 0};
+    Vector2 acc = {0, GRAVITY_PPF2};
+
+    while (!WindowShouldClose()){
+        //update
+        vel.x += acc.x;
+        vel.y += acc.y;
+
+        pos.x += vel.x;
+        pos.y += vel.y;
+
+        for (int i = 0; i < num_lines; i++){
+            if(CheckCollisionPointLine(pos, shapes[i], shapes[i+1], LINE_THICKNESS + BALL_SIZE)){
+                vel.x = vel.x;
+                vel.y = -vel.y;
+            }
+        };
+
+
+        //render
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawCircleV(pos, BALL_SIZE, BLUE);
+
+        for (int i = 0; i < num_lines; i++){
+            DrawLineEx(shapes[i], shapes[i+1], LINE_THICKNESS, LINE_COLOR);
+        };
+
         EndDrawing();
     }
     CloseWindow();
