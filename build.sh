5fcf8711d40dcd450598fe876e18867cac91c26b
Takeoischifan <takeoischifan@github.com>
2023-10-17 15:10:26 +0200

    raylib window

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 set -xe
 
+# C compiler
 CC="clang"
-CFLAGS="-Wall -Wextra"
-LIBS=""
+
+# Flags for the C compiler.
+# pkg-config is a platform-agnostic tool that finds installed packages on your system. In this case, we add raylibs cflags using pkgconfig.
+CFLAGS="-Wall -Wextra `pkg-config --cflags raylib`"
+
+# Link with raylib and math
+LIBS="-lm `pkg-config --libs raylib`"
 SRC="main.c"
 
 $CC $CFLAGS -o raysmr $SRC $LIBS
+
 
 
 
--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "raylib.h"
+
+#define SCREEN_SIZE 800
+#define TARGET_FPS 60
+
 
 int main(void){
-    printf("Hello,Mom!\n");
+
+    InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Raysmr");
+    SetTargetFPS(TARGET_FPS);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
+
     return 0;
+
 }
